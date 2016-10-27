63bcbc944e38c8d3e0aa5d886bb71c38427d5db2
Fernando Homem da Costa <nandohdc@gmail.com>
2016-10-27 01:21:11 -0200

    add: login screen

---
+++ web/Front-End/include/header.php
@@ -0,0 +1,9 @@
+<!-- header.php -->
+<header>
+	<div class="wrapper">
+		<div id="logo">
+			<!-- Redirecting to home(Log In Page) -->
+			<a href="../img/RESPUCLOGO.gif">RESPUC - Log In Restricted Area</a>
+		</div><!-- End: Div Logo -->	
+	</div><!-- End: Div Wrapper -->
+</header>

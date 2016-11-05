c5fd87e240c129fa9c0bd574fc2e14a226b88343
Fernando Homem da Costa <nandohdc@gmail.com>
2016-11-05 11:15:24 -0200

    updating consults

---
+++ web/include/main_home.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<h2>Sobre a RESPUC</h2>
+	<p>A RESPUC (Rede de Empreendimentos Sociais da PUC-Rio),  tem como objetivo reunir os projetos criados, desenvolvidos e mantidos na universidade, ou por membros de sua comunidade.</p>
+
+	<p>Considerando que as atividades desenvolvidas em empreendimentos sociais podem potencializar a reflexão e o senso crítico e a responsabilidade social, incorporando valores morais e princípios éticos e, portanto, proporcionar a formação de um profissional cidadão, a Vice-Reitoria para Assuntos Comunitários (VRC) instituiu o Programa RESPUC Ação Comunitária , tendo como finalidade precípua o desenvolvimento pessoal, ético, cívico e social, por meio do serviço voluntário em prol da inclusão social.</p>
+
+
+	<p>Localizada na PUC-Rio, Rio de Janeiro, sua missão é trocar experiências e conhecimentos, potencializar o impacto social gerado pelos empreendimentos, estabelecendo um canal de diálogo com a sociedade e incentivar a participação social, junto à solidariedade comunitária.</p>
+
+
+	<p>Atualmente, os membros desta rede utilizam uma planilha em Excel para controlar os formulários enviados e realizar o planejamento da RESPUC. Porém, enfrentam dificuldades em armazenar o volume de informações que se atualizam constantemente, além de não aplicar várias regras relevantes. Por isso, desejamos propor uma solução em banco de dados, ou seja, um sistema para administrar e acompanhar os alunos que se inscrevem para ser voluntários das instituições.</p>
+	
+	<h2>Nosso Objetivo</h2>
+	<p>É gerar uma solução para RESPUC, com o uso de aplicação web e um banco de dados que irá fazer com que essas as principais gestões, Aluno, Projeto e Instituições, não percam informações cruciais para consultas futuras.</p>
+	
+	<p>Dessa forma, irá facilitar o dia-a-dia dos membros, permitindo a rápida e precisa obtenção de informações, relatório mais detalhados e um melhor gerenciamento da empresa. Para conseguir ter um bom controle dos dados, é necessário um breve planejamento, que será apresentado a seguir.</p>
+	
+</div>

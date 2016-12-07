5ef5b6e0ad264a35e34a5af286d79c6527634552
Fernando Homem da Costa <nandohdc@gmail.com>
2016-12-07 00:49:20 -0200

    Finished

--- web/LogIn/arearestrita/main_arearestrita.php
+++ web/LogIn/arearestrita/main_arearestrita.php
@@ -6,7 +6,6 @@
 					<option value="" selected>Escolha uma consulta</option>
 					<option value="1">Todos os alunos inscritos por curso.</option>
 					<option value="2">Todos os alunos com inscrição aprovada.</option>
-					<option value="3">Todos os cursos dos alunos com inscrição aprovada.</option>
 					<option value="4">Todos os alunos por gênero</option>
 					<option value="5">Todos os projeto que tem pelo menos um aluno.</option>
 					<option value="6">Todos os alunos inscritos.</option>
